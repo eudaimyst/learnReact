17cfbbc944d7b05e639637de1156a4df4d2aae3e
skipgamer <tpilepich@gmail.com>
2023-12-20 16:36:07 +1100

    dumb bug 8th row is showing as white pieces?

--- src/App.js
+++ src/App.js
@@ -8,14 +8,15 @@ let selectedSquare = null;
 let selectedPiece = null;
 
 const gameStates = { awaitingSelection: {text: 'awaiting selection'}, awaitingPlacement: {text: 'awaiting placement'} }
+const sides = { white: {text: 'White', color: 'white'}, black: {text: 'Black', color: 'black'} }
 
 const Game = {
-	turn: 'white',
+	turn: sides.white,
 	turnCount: 1,
 	state: gameStates.awaitingSelection,
 	NextTurn: function() {
-		if (Game.turn == 'white') Game.turn = 'black';
-		else Game.turn = 'white';
+		if (Game.turn == sides.white) Game.turn = sides.black;
+		else Game.turn = sides.white;
 		Game.turnCount++;
 	},
 	SelectPiece: function(square) {
@@ -38,42 +39,77 @@ const pieces = {
 		name: 'King',
 		side: null,
 		position: null,
+		movementRules: {
+			directions: [ {x: 1, y: 0},{x: 1, y: 1},{x: 0, y: 1},{x: -1, y: 1},{x: -1, y: 0},{x: -1, y: -1},{x: 0, y: -1},{x: 1, y: -1} ],
+			count: 1,
+			jumps: false
+		}
 	},
 	queen: {
 		notation: 'Q',
 		name: 'Queen',
 		side: null,
 		position: null,
+		movementRules: {
+			directions: [ {x: 1, y: 0},{x: 1, y: 1},{x: 0, y: 1},{x: -1, y: 1},{x: -1, y: 0},{x: -1, y: -1},{x: 0, y: -1},{x: 1, y: -1} ],
+			count: 7,
+			jumps: false
+		}
 	},
 	bishop: {
 		notation: 'B',
 		name: 'Bishop',
 		side: null,
 		position: null,
+		movementRules: {
+			directions: [ {x: 1, y: 1},{x: -1, y: 1},{x: -1, y: -1},{x: 1, y: -1} ],
+			count: 7,
+			jumps: false
+		}
 	},
 	rook: {
 		notation: 'R',
 		name: 'Rook',
 		side: null,
 		position: null,
+		movementRules: {
+			directions: [ {x: 1, y: 0},{x: -1, y: 0},{x: 0, y: 1},{x: 0, y: -1} ],
+			count: 7,
+			jumps: false
+		}
 	},
 	whitePawn: {
 		notation: 'P',
 		name: 'Pawn',
-		side: 'white',
+		side: sides.white,
 		position: null,
+		movementRules: {
+			directions: [ {x: 0, y: 1} ],
+			count: 1,
+			jumps: false
+		}
 	},
 	blackPawn: {
 		notation: 'P',
 		name: 'Pawn',
-		side: 'black',
+		side: sides.black,
 		position: null,
+		movementRules: {
+			directions: [ {x: 0, y: -1} ],
+			count: 1,
+			jumps: false
+		}
 	},
 	knight: {
 		notation: 'N',
 		name: 'Knight',
 		side: null,
 		position: null,
+		movementRules: {
+			directions: [ {x: 1, y: 2}, {x: 2, y: 1}, {x: 2, y: -1}, {x: 1, y: -2}, {x: -1, y: -2}, {x: -2, y: -1}, {x: -2, y: 1}, {x: -1, y: 2} ],
+			count: 1,
+			jumps: true
+		}
 	}
 }
 
@@ -95,10 +131,10 @@ const startingSquares = {
 const files = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 
 export default function App() {
-	const turnDisp = <p key = 'turnDisp'>Turn: {Game.turnCount+", "+Game.turn}</p>;
+	const turnDisp = <p key = 'turnDisp'>Turn: {Game.turnCount+", "+Game.turn.text}</p>;
 	const stateDisp = <p key = 'stateDisp'>State: {Game.state.text}</p>;
 	const selectedSquareDisp = <p key = 'selectedSquareDisp'>selectedSquare:{selectedSquare}</p>;
-	const selectedPieceDisp = <p key = 'selectedPieceDisp'> selectedPiece:{selectedPiece ? selectedPiece.side+' '+selectedPiece.name:''}</p>; // (if) x ?(then) y :(else) z 
+	const selectedPieceDisp = <p key = 'selectedPieceDisp'> selectedPiece:{selectedPiece ? selectedPiece.side.text+' '+selectedPiece.name:''}</p>; // (if) x ?(then) y :(else) z 
 	//const currentPiece = <h1 key = 'cph'>current piece:{previousPiece}</h1>;
 
 	let renderArray = [] //elements are added to this which get returned to the document root
@@ -115,7 +151,7 @@ function GameBoard() {
 		if (i % 2 === 0) boardArray.push(BoardRow(1, i));
 		else boardArray.push(BoardRow(0, i));
 	}
-	//console.log(boardArray); //debug
+	console.log(boardArray); //debug
 	return boardArray;
 }
 
@@ -129,6 +165,7 @@ function BoardRow(alt, count) {
 }
 
 function Square(dark, row, column) {
+	//console.log(boardPieces)
 	let className; 
 	const rank = 8-row;
 	const file = files[column];
@@ -136,20 +173,23 @@ function Square(dark, row, column) {
 	let pieceString;
 	if (!boardPieces[square]) { //if the board pieces have not been intialised
 		boardPieces[square] = {
-			currentPiece: null, //ensures boardPieces table entry is initialised at index of square as key with currentPiece property (even if nil)
+			currentPiece: null, //ensures boardPieces table entry is initialised at index square with currentPiece property (even if nil)
 		}
 		//places the piece data from the startingSquares table into the boardPieces table
 		if (startingSquares.white[square]) {
 			boardPieces[square].currentPiece = startingSquares.white[square];
-			boardPieces[square].currentPiece.side = 'white'; //set the correct side for the piece
+			boardPieces[square].currentPiece.side = sides.white; //set the correct side for the piece
+			console.log('adding white piece '+square+', '+boardPieces[square].currentPiece.notation);
+			console.log(boardPieces[square].currentPiece.side);
 		}
 		else if (startingSquares.black[square]) {
 			boardPieces[square].currentPiece = startingSquares.black[square];
-			boardPieces[square].currentPiece.side = 'black';
+			boardPieces[square].currentPiece.side = sides.black;
+			console.log('adding black piece '+square+', '+boardPieces[square].currentPiece.notation);
+			console.log(boardPieces[square].currentPiece.side);
 		}
 	}
 	const [renderTrigger, triggerUpdate] = useState('');
-	//const [piece, setPiece] = useState(pieceString)
 
 	if (dark) className = 'squareDark';
 	else className = 'squareLight';
@@ -160,9 +200,9 @@ function Square(dark, row, column) {
 		selectedSquare = square;
 		let clickedPiece = null;
 		if (boardPieces[square].currentPiece) {
-			//clickedPiece = boardPieces[square].currentPiece;
+			clickedPiece = boardPieces[square].currentPiece;
+			console.log(clickedPiece.side);
 			Game.SelectPiece(square);
-			//console.log('piece: ' + clickedPiece.notation + ' clicked');
 		}
 		/*
 		if (clickedPiece) {
@@ -183,7 +223,7 @@ function Square(dark, row, column) {
 			};
 		}
 		*/
-		triggerUpdate(trigger => trigger + ' ');
+		triggerUpdate(trigger => square);
 	}
 
 	let content = ''
